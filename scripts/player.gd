ac3f8ee9b8810e18bb36bf2766395aaee7a45032
Eureka3a <109967845+Eureka3a@users.noreply.github.com>
2026-05-26 00:33:31 +0800

    Optimized the character animations..
    1. 可自选落地动画打断
    2. 滑墙落地切换到落地动画
    3. 使用2个RayCast2D节点判断角色是否能够触发滑墙动画。
    4. 删除落地动画第一帧，时长调整为0.2s

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ enum State {
 	JUMP,
 	FALL,
 	LANDING,
+	WALL_SLIDING,
 }
 
 const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
@@ -21,6 +22,8 @@ var is_first_tick := false
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var jump_request_timer: Timer = $JumpRequestTimer
+@onready var hand_checker: RayCast2D = $Graphcs/HandChecker
+@onready var foot_checker: RayCast2D = $Graphcs/FootChecker
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
@@ -44,6 +47,10 @@ func tick_physics(state: State, delta: float) -> void:#在_physics_process上修
 			
 		State.LANDING:
 			stand(delta) #下落动画播放时不接收玩家输入
+			
+		State.WALL_SLIDING:
+			move(default_gravity / 3, delta)
+			graphcs.scale.x = get_wall_normal().x #返回最近一次碰撞点的法线
 			
 	is_first_tick = false
 			
@@ -95,10 +102,21 @@ func get_next_state(state: State) -> State:
 		State.FALL:
 			if is_on_floor():
 				return State.LANDING if is_still else State.RUNNING
+			if is_on_wall() and hand_checker.is_colliding() and foot_checker.is_colliding(): #玩家与墙碰撞且墙与头脚部碰撞才进入滑墙状态
+				return State.WALL_SLIDING
 				
 		State.LANDING:
+			#if not is_still:
+				#return	State.RUNNING #取消落地硬直
 			if not animation_player.is_playing():
 				return State.IDLE
+				
+		State.WALL_SLIDING:
+			if is_on_floor():
+				return State.LANDING #切换成IDLE有些生硬
+			if not is_on_wall():
+				return State.FALL
+				
 		
 	return state
 
@@ -126,5 +144,8 @@ func transition_state(from: State, to: State) -> void:
 				
 		State.LANDING:
 			animation_player.play("landing")
+			
+		State.WALL_SLIDING:
+			animation_player.play("wall_sliding")
 		
 	is_first_tick = true
