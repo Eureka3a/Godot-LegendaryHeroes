420071ba5cbc8cc1a3ce0f94d9209f4dc5f84912
Eureka3a <109967845+Eureka3a@users.noreply.github.com>
2026-06-11 00:02:03 +0800

    临时保存，为了安装插件

---
+++ scripts/classes/Hitbox.gd
@@ -0,0 +1,14 @@
+class_name Hitbox
+extends Area2D
+
+signal hit(hurtbox)
+
+
+func _init() -> void:
+	area_entered.connect(_on_area_entered)#传递_on_area_entered函数本身，而非返回值
+
+
+func _on_area_entered(hurtbox: Hurtbox) -> void:
+	print("[Hit] %s => %s" % [owner.name, hurtbox.owner.name] )
+	hit.emit(hurtbox)
+	hurtbox.hurt.emit(self)
